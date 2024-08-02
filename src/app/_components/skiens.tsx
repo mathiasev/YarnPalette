6d3534733f48e5188a79c060f4ede59a854a326f
Mathias Everson <mathias@mathiaseverson.com>
2024-08-02 20:56:24 +1000

    Household Updates

--- src/app/_components/skiens.tsx
+++ src/app/_components/skiens.tsx
@@ -12,7 +12,7 @@ export function Skiens() {
   const placeholder = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
-    <div className="relative mx-auto px-2 w-[90vw] xl:max-w-screen-lg xl:w-auto  grid grid-cols-2 xl:grid-cols-4 gap-12 ">
+    <div className="relative mx-auto px-2 w-[90vw] xl:max-w-screen-lg xl:w-auto  grid grid-cols-1 xl:grid-cols-4 gap-12 ">
       {yourSkiens.isLoading && placeholder.map((_, i) => (
         <div key={i} className="grid gap-y-2 ">
           <Skeleton className="h-80 w-56 rounded" />
@@ -24,10 +24,10 @@ export function Skiens() {
         <Link href={`/skiens/${skien.id}`} key={skien.id} className="grid gap-y-2 relative">
           <Badge className="absolute right-2 top-2" >{skien.skienStocks?.reduce((acc, curr) => acc + curr.stock, 0)}</Badge>
           {!skien.imageUrl && <Skeleton className="h-80 w-56 rounded bg-muted" />}
-          {skien.imageUrl && <Image src={skien.imageUrl} alt={skien.name} width={224} height={320} className="h-80 w-56 rounded object-cover max-w-none" />}
+          {skien.imageUrl && <Image src={skien.imageUrl} alt={skien.name} width={224} height={320} className="h-80 w-56  rounded object-cover max-w-none" />}
 
           <h3 className="font-medium leading-none">{skien.name}</h3>
-          <p className="text-xs text-muted-foreground">{skien.createdAt.toLocaleDateString('en-AU')}</p>
+          <p className="text-xs text-muted-foreground">{skien.createdAt.toLocaleDateString('en-AU')} | {skien.organization ? 'Household' : 'Private'}</p>
         </Link>
       ))}
 
